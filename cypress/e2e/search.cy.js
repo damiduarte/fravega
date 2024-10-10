56565716075d25306ba7ee2d24dc79d44953f501
Damian <damian.duarte@tiendamia.com>
2024-10-10 20:50:43 -0300

    Parametrización de valores

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,12 +1,12 @@
 //Agregar contenedor anterior a contains
 const searchPage = require('../pages/searchPage.js');
 const headerPage = require('../pages/headerPage.js');
-
-const searchTerm = 'heladeras';
-const filterTitle = 'Marca';
+const searchTerm = data.searchTerm;
+import data from '../fixtures/data.json';
 
 describe('Search', () => {
   it('Buscar heladeras, filtrar por primer marca de filtros y validar filtro aplicado', () => {
+    const filterTitle = 'Marca';
     cy.visit('/');
 
     cy.closePopup();
