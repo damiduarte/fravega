7cb62ac03c5073b5f28a94f6a27601e55a157fd3
Damian <damian.duarte@tiendamia.com>
2024-10-12 14:53:21 -0300

    Fixes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-Para ejecutar el script seguir los siguientes pasos:
+Para ejecutar las pruebas se deben seguir los siguientes pasos:
+- git clone https://github.com/damiduarte/fravega.git
 - npm install
 - npx cypress run --spec 'cypress/e2e/search.cy.js'
--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,24 +1,24 @@
 const searchPage = require('../pages/searchPage.js');
 const headerPage = require('../pages/headerPage.js');
 const reportPath = 'cypress/reports/';
-var reportFileName;
+let reportFileName;
 
 describe('Search', () => {
   beforeEach(() => {
     cy.visit('/');
-    reportFileName = reportPath + this.test.title + '.txt';
   })
 
   it('Buscar heladeras, filtrar por primer marca de filtros y validar filtro aplicado', () => {
-    const filterTitle = 'Marca';
     cy.closePopup();
-
     headerPage.makeSearch('heladeras');
+
+    const filterTitle = 'Marca';
     searchPage.getCheckboxFiltersFirstValue(filterTitle).click();
     searchPage.validateAllPagesAppliedFilter();
   })
 
   afterEach(() => {
+    reportFileName = reportPath + Cypress.currentTest.title + '.txt';
     cy.checkErrors(reportFileName);
   })
 })
